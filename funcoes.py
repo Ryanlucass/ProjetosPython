2b10fca1c54b55313a8a0f9937cb9bd0416e96a8
Lucas alves <lucasryanalves@gmail.com>
2020-12-10 05:46:50 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,31 @@
 n1=10
-n2=5
+n2=9
 def somar():
     r=n1+n2
-    print(r)
     
-somar()
+    print("A Soma de " + str(n1) + " e " + str(n2) + " = " +str(r))
+    
+def subtrair():
+    r=n1-n2
+    
+    print("A subtração de " + str(n1) + " e " + str(n2) + " = " +str(r))
+
+def multiplicar():
+
+    r=n1*n2
+    
+    print("A multiplicação de " + str(n1) + " e " + str(n2) + " = " +str(r))
+
+def divisao():
+
+    r=n1/n2
+    
+    print("A divisão de " + str(n1) + " e " + str(n2) + " = " +str(r))
     
 
+somar()
+subtrair()
+multiplicar()
+divisao()
 
 # eu precisei chamar a função, para então ele me mostrar seus objetos 
